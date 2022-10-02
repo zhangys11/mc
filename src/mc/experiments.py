dd8fc0e3d7183f128759f9c4e20c173f0b82ce2c
zhangys11 <oo@zju.edu.cn>
2022-10-02 15:26:10 +0800

    0.0.2

--- src/mc/experiments.py
+++ src/mc/experiments.py
@@ -1,6 +1,14 @@
 import numpy as np
 import random
 from tqdm import tqdm
+import collections
+import matplotlib.pyplot as plt
+from IPython.display import HTML, display
+
+if __package__:
+    from . import distributions
+else:
+    import distributions
 
 def pi(N = 1000000, flavor = 0):
     
@@ -21,6 +29,20 @@ def pi(N = 1000000, flavor = 0):
     """
 
     if flavor == 1:
+
+        def is_inside_unit_circle(x, y):
+            '''
+            Determines whether the point (x,y) falls inside the unit circle.   
+            '''
+            return x**2 + y**2 < 1
+
+        def unit_test():
+            assert is_inside_unit_circle(0.5,-0.5) == True
+            assert is_inside_unit_circle(0.999,0.2) == False
+            assert is_inside_unit_circle(0,0) == True
+            assert is_inside_unit_circle(0.5,-0.5) == True
+            assert is_inside_unit_circle(-0.9,0.9) == False   
+    
         xs = np.random.uniform(-1,1,N)
         ys = np.random.uniform(-1,1,N)
         # cnt 用来统计落在圆内点的数目
@@ -44,20 +66,7 @@ def pi(N = 1000000, flavor = 0):
     print("PI = {}".format(idx.sum()/N*4))        
     return freq, PI
           
-def is_inside_unit_circle(x, y):
-    '''
-    Determines whether the point (x,y) falls inside the unit circle.   
-    '''
-    return x**2 + y**2 < 1
 
-def unit_test():
-    assert is_inside_unit_circle(0.5,-0.5) == True
-    assert is_inside_unit_circle(0.999,0.2) == False
-    assert is_inside_unit_circle(0,0) == True
-    assert is_inside_unit_circle(0.5,-0.5) == True
-    assert is_inside_unit_circle(-0.9,0.9) == False
-    
-    
 def parcel(N=100000, num_players = 5, num_ops = 10):
     
     """
@@ -102,27 +111,27 @@ def parcel(N=100000, num_players = 5, num_ops = 10):
     return L/N
 
 
-def dices(N = 10000000):
+def dices(N = 10000):
     """
-   Randomly roll three dice and calculate the probabilities of various situations. 
-   The corresponding score for each dice point is as follows:
-   If the three dice have 1, 2, 3 points or 4, 5 and 6 respectively, 16 points are awarded;
-   If all three dice have the same number of points, 8 points are awarded;
-   If only two of the dice have the same number of points, 2 points are awarded;
-   If the three dice have different points, 0 points are awarded.
+    Randomly roll three dice and calculate the probabilities of various situations. 
+    The corresponding score for each dice point is as follows:
+    If the three dice have 1, 2, 3 points or 4, 5 and 6 respectively, 16 points are awarded;
+    If all three dice have the same number of points, 8 points are awarded;
+    If only two of the dice have the same number of points, 2 points are awarded;
+    If the three dice have different points, 0 points are awarded.
 
-   Parameters
-   ----------
-   N : number of experiments.
+    Parameters
+    ----------
+    N : number of experiments.
 
-   Returns
-   -------
-   The number of times each case occurs and verifies that the final result satisfies the normalization.
+    Returns
+    -------
+    Frequency History, i.e., the number of times each case occurs
 
-   Notes
-   -----
-   When calculating the probability that the three dices have different points.
-   it is necessary to remove the cases where the number of points is 1, 2, 3, and 4, 5, and 6, respectively.
+    Notes
+    -----
+    When calculating the probability that the three dices have different points.
+    it is necessary to remove the cases where the number of points is 1, 2, 3, and 4, 5, and 6, respectively.
     """
 
     samples = np.random.randint(low=1, high=7, size=(N,3)) # range: [low, high)
@@ -132,7 +141,8 @@ def dices(N = 10000000):
     dict_cnt['456'] = 0
     dict_cnt['xyz'] = 0 # 三个均不同，但需排除123和456的情况
     dict_cnt['oox'] = 0 # 两个相同
-    for s in tqdm(samples):
+
+    for s in samples:
         if s[0] == s[1] and s[0] == s[2]:
             dict_cnt['ooo'] += 1 # 三个相同
         elif sorted(s) == [1,2,3]:
@@ -143,29 +153,54 @@ def dices(N = 10000000):
             dict_cnt['xyz'] += 1 # 三个均不同，但需排除123和456的情况
         else:
             dict_cnt['oox'] += 1# 两个相同
-    sum=dict_cnt['ooo'] + dict_cnt['xyz'] + dict_cnt['123'] + dict_cnt['456'] + dict_cnt['oox']
-    print(dict_cnt)
-    print(dict_cnt['ooo']/sum + dict_cnt['xyz']/sum + dict_cnt['123']/sum + dict_cnt['456']/sum + dict_cnt['oox']/sum)# 满足归一化
-    
+
+    assert(dict_cnt['ooo'] + dict_cnt['xyz'] + dict_cnt['123'] + dict_cnt['456'] + dict_cnt['oox'] == N)
+
+    for key in dict_cnt:
+        dict_cnt[key] = dict_cnt[key] / N
+
+    # print("Experiment Result", dict_cnt)
+
     #  Theoretical value：
-    dict_cnt = {}
-    dict_cnt['ooo'] = 6*(1/6)**3
-    dict_cnt['123'] = 6*(1/6)**3
-    dict_cnt['456'] = 6*(1/6)**3
-    dict_cnt['xyz'] = 6*5*4/(6**3)-dict_cnt['123']-dict_cnt['456']
-    dict_cnt['oox'] = 6*5*3/(6**3)
-    print("Theoretical value：",dict_cnt)
-    print("Theoretical value：",dict_cnt['ooo'] + dict_cnt['xyz'] + dict_cnt['123'] + dict_cnt['456'] + dict_cnt['oox']) # 满足归一化
+    dict_tcnt = {}
+    dict_tcnt['ooo'] = 6*(1/6)**3
+    dict_tcnt['123'] = 6*(1/6)**3
+    dict_tcnt['456'] = 6*(1/6)**3
+    dict_tcnt['xyz'] = 6*5*4/(6**3)-dict_cnt['123']-dict_cnt['456']
+    dict_tcnt['oox'] = 6*5*3/(6**3)
 
-    return 
+    html_str = '<h2>The dice experiment</h2><p>' + '''
+    Randomly roll three dice and calculate the probabilities of various situations. 
+    The corresponding score for each dice combination is as follows:
+    (1) If the three dice have 1, 2, 3 points or 4, 5 and 6 respectively, 16 points are awarded;
+    (2) If all three dice have the same number of points, 8 points are awarded;
+    (3) If two of the dices have the same number of points, 2 points are awarded;
+    (4) If the three dices have different points, 0 points are awarded.
+    ''' + '</p>'
+    html_str += '<table>'
+    html_header = '<tr><th></th>'
+    html_row1 = '<tr><td>Experimental Frequencies (f)<br/>N = ' + str(N) + '</td>'
+    html_row2 = '<tr><td>Theoretical PMF (p)</td>'
 
+    for key in dict_tcnt:
+        html_header += '<th>'+ key + '</th>'
+        dict_tcnt[key] = round( dict_tcnt[key] , 6)
+        html_row1 += '<td>' + str(dict_cnt[key]) + '</td>'
+        html_row2 += '<td>' + str(dict_tcnt[key]) + '</td>'
 
-def galton_board(m = 20, N = 5000, display = True):
+    # print("Theoretical PMF",dict_tcnt)
+
+    html_str = html_str + html_header + '</tr>' + html_row1 + '</tr>' + html_row2 + '</tr>' + '</table>'
+    display(HTML(html_str))
     
-    return binom(m, N, display = True) 
+    return dict_cnt
 
+def galton_board(num_layers = 20, N = 5000, flavor=1):
+    return distributions.binom(num_layers, N, flavor) 
 
 def paper_clips(num_rounds = 10000, num_clips_k = 1.6, verbose = False):
-    
-    return zipf(num_rounds, num_clips_k, verbose = False)
+    return distributions.zipf(num_rounds, num_clips_k, verbose)
+
+def sudden_death(N= 1000, p=0.01):
+    return distributions.exponential(N, p)
 
